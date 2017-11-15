ebd3db5fd87cbf7846e37676cc5732a819507fcb
Osman_Brito <osman.gimenes@gmail.com>
2017-11-15 16:57:21 -0200

    Initial commit, telas iniciais.

---
+++ app/src/main/java/com/example/unhee/telasapp/Tela2.java
@@ -0,0 +1,17 @@
+package com.example.unhee.telasapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by unhee on 15/11/17.
+ */
+
+public class Tela2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela2);
+    }
+}
